3c7d88a3daab77e494a59617139673cde9b7fc95
fedecek22 <fedecek3@gmail.com>
2020-08-14 01:54:45 +0200

    Added TimeOffset to event parser

--- Lib/EventParser.js
+++ Lib/EventParser.js
@@ -3,21 +3,22 @@ class EventParser {
         this.Server = Server
     }
     getEventData(eventString) {
-        eventString = eventString.replace(/[0-9]+:[0-9]+\s/g, '').trim();
+        eventString = eventString.trim().replace(new RegExp(/([0-9]+:[0-9]+)\s+/g), '$1;')
         var eventRegex = {
           /* https://github.com/RaidMax/IW4M-Admin/blob/2.4-pr/Application/EventParsers/BaseEventParser.cs :) */
-          say: /^(say|sayteam);(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0);([0-9]+);([^;]*);(.*)$/g,
-          join: /^(J);(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0);([0-9]+);(.*)$/g,
-          quit: /^(Q);(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0);([0-9]+);(.*)$/g,
-          damage: /^(D);(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0);(-?[0-9]+);(axis|allies|world|none)?;([^;]{1,24});(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0)?;(-?[0-9]+);(axis|allies|world|none)?;([^;]{1,24})?;((?:[0-9]+|[a-z]+|_|\+)+);([0-9]+);((?:[A-Z]|_)+);((?:[a-z]|_)+)$/g,
-          kill: /^(K);(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0);(-?[0-9]+);(axis|allies|world|none)?;([^;]{1,24});(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0)?;(-?[0-9]+);(axis|allies|world|none)?;([^;]{1,24})?;((?:[0-9]+|[a-z]+|_|\+)+);([0-9]+);((?:[A-Z]|_)+);((?:[a-z]|_)+)$/g,
-          init: /^InitGame$/g
+          say: /^([0-9]+:[0-9]+);(say|sayteam);(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0);([0-9]+);([^;]*);(.*)$/g,
+          join: /^([0-9]+:[0-9]+);(J);(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0);([0-9]+);(.*)$/g,
+          quit: /^([0-9]+:[0-9]+);(Q);(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0);([0-9]+);(.*)$/g,
+          damage: /^([0-9]+:[0-9]+);(D);(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0);(-?[0-9]+);(axis|allies|world|none)?;([^;]{1,24});(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0)?;(-?[0-9]+);(axis|allies|world|none)?;([^;]{1,24})?;((?:[0-9]+|[a-z]+|_|\+)+);([0-9]+);((?:[A-Z]|_)+);((?:[a-z]|_)+)$/g,
+          kill: /^([0-9]+:[0-9]+);(K);(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0);(-?[0-9]+);(axis|allies|world|none)?;([^;]{1,24});(-?[A-Fa-f0-9_]{1,32}|bot[0-9]+|0)?;(-?[0-9]+);(axis|allies|world|none)?;([^;]{1,24})?;((?:[0-9]+|[a-z]+|_|\+)+);([0-9]+);((?:[A-Z]|_)+);((?:[a-z]|_)+)$/g,
+          init: /^([0-9]+:[0-9]+);InitGame$/g
         }
         var eventData = {type: null, data: null}
         Object.entries(eventRegex).forEach((r) => {
           if (eventString.match(r[1])) {
             var eventVars = eventString.split(';')
-            eventData = {type: r[0], vars: eventVars.slice(1)}
+            eventVars[0] = parseInt(eventVars[0].split(':')[0]) * 60 + parseInt(eventVars[0].split(':')[1]),
+            eventData = {type: r[0], vars: eventVars}
           }
         })
         return eventData;
@@ -30,36 +31,39 @@ class EventParser {
         switch (eventData.type) {
             case 'say':
                 parsedEvent.data = {
-                    Origin: this.Server.Clients[eventData.vars[1]],
-                    Message: eventData.vars[3]
+                    TimeOffset: eventData.vars[0],
+                    Origin: this.Server.Clients[eventData.vars[3]],
+                    Message: eventData.vars[5]
                 }
             break;
             case 'quit':
             case 'join':
                 parsedEvent.data = {
+                    TimeOffset: eventData.vars[0],
                     Origin: {
-                        Guid: eventData.vars[0],
-                        Clientslot: eventData.vars[1],
-                        Name: eventData.vars[2]
+                        Guid: eventData.vars[2],
+                        Clientslot: eventData.vars[3],
+                        Name: eventData.vars[4]
                     },
                 }
             break;
             case 'kill':
             case 'damage':
-                var Weapon = eventData.vars[8]
+                var Weapon = eventData.vars[10]
                 var BaseWeapon = Weapon
-                if (Weapon.indexOf('_mp_') > 0) {
+                if (Weapon.indexOf('_mp') > 0) {
                     BaseWeapon = Weapon.substr(0, Weapon.indexOf('_mp'))
                 }
-                var suicide = eventData.vars[1] == eventData.vars[5]
+                var suicide = eventData.vars[3] == eventData.vars[7]
                 parsedEvent.data = {
-                    Target: this.Server.Clients[eventData.vars[1]],
-                    Origin: suicide ? {ClientId: 1} : this.Server.Clients[eventData.vars[5]],
+                    TimeOffset: eventData.vars[0],
+                    Target: this.Server.Clients[eventData.vars[3]],
+                    Origin: suicide ? {ClientId: 1} : this.Server.Clients[eventData.vars[7]],
                     Attack: {
-                        Weapon: eventData.vars[8],
-                        Damage: eventData.vars[9],
-                        MOD: eventData.vars[10],
-                        HitLoc: eventData.vars[11],
+                        Weapon: eventData.vars[10],
+                        Damage: eventData.vars[11],
+                        MOD: eventData.vars[12],
+                        HitLoc: eventData.vars[13],
                         BaseWeapon: BaseWeapon
                     }
                 } 
