5dd3b1e3a32e5c09b4be085fed5b5fee6bfa6dad
Federico Cecchetto <fedecek3@gmail.com>
2020-10-09 23:31:38 +0200

    Catch errors on Penalties plugin

--- Plugins/Penalties.js
+++ Plugins/Penalties.js
@@ -9,21 +9,24 @@ class Plugin {
         this.Server.on('any_event', this.onEvent.bind(this))
     }
     async onEvent (Event) {
-        var playerPenalties = await this.Server.DB.getAllPenalties(Event.Origin.ClientId)
+        try {
+            var playerPenalties = await this.Server.DB.getAllPenalties(Event.Origin.ClientId)
 
-        for (var i = 0; i < playerPenalties.length; i++) {
-            switch (playerPenalties[i].PenaltyType) {
-                case 'PENALTY_PERMA_BAN':
-                    playerPenalties[i].Active && Event.Origin.Kick(`Banned for: ^5${playerPenalties[i].Reason}`, NodeServerManager)
-                break
-                case 'PENALTY_TEMP_BAN':
-                    var dateDiff = (new Date(playerPenalties[i].Date) - new Date()) / 1000
-                    if (dateDiff + playerPenalties[i].Duration > 0) {
-                        playerPenalties[i].Active && Event.Origin.Kick(`Banned for: ^5${playerPenalties[i].Reason}^7 ${Utils.secondsToDhms(dateDiff + playerPenalties[i].Duration)} left`, NodeServerManager)
-                    }
-                break
+            for (var i = 0; i < playerPenalties.length; i++) {
+                switch (playerPenalties[i].PenaltyType) {
+                    case 'PENALTY_PERMA_BAN':
+                        playerPenalties[i].Active && Event.Origin.Kick(`Banned for: ^5${playerPenalties[i].Reason}`, NodeServerManager)
+                    break
+                    case 'PENALTY_TEMP_BAN':
+                        var dateDiff = (new Date(playerPenalties[i].Date) - new Date()) / 1000
+                        if (dateDiff + playerPenalties[i].Duration > 0) {
+                            playerPenalties[i].Active && Event.Origin.Kick(`Banned for: ^5${playerPenalties[i].Reason}^7 ${Utils.secondsToDhms(dateDiff + playerPenalties[i].Duration)} left`, NodeServerManager)
+                        }
+                    break
+                }
             }
         }
+        catch (e) {}
     }
 
 }
