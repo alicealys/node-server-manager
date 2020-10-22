a4a06626c68f62bd2e76c8adae1fd441342e9532
Federico Cecchetto <fedecek3@gmail.com>
2020-10-22 19:10:52 +0200

    Remove useless function

--- Lib/CLICommands.js
+++ Lib/CLICommands.js
@@ -9,15 +9,6 @@ const rl = readline.createInterface({
     terminal: false
 })
 
-function setTerminalTitle(title)
-{
-  process.stdout.write(
-    String.fromCharCode(27) + "]0;" + title + String.fromCharCode(7)
-  );
-}
-
-setTerminalTitle('NODE SERVER MANGER')
-
 class CLICommands {
     constructor(Manager, Managers) {
         this.Managers = Managers
