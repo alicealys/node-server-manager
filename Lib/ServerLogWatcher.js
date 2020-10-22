8ba5c7c35091d2b75757855801a78eecd6d43635
fedddddd <fedecek3@gmail.com>
2020-10-22 22:28:40 +0000

    Update ServerLogWatcher

--- Lib/ServerLogWatcher.js
+++ Lib/ServerLogWatcher.js
@@ -1,10 +1,6 @@
-const EventParser   = require('./EventParser.js')
-const ePlayer       = require('./Entity/ePlayer.js')
-const md5           = require('md5')
-const fs            = require('fs')
-const readLastLines = require('read-last-lines')
-const ws            = require('ws')
-const _EventDispatcher = require('./EventDispatcher.js')
+const EventParser       = require('./EventParser.js')
+const ws                = require('ws')
+const _EventDispatcher  = require('./EventDispatcher.js')
 
 class EventLogWatcher extends EventParser {
     constructor (logServerURI, Server, Manager) {
@@ -19,14 +15,24 @@ class EventLogWatcher extends EventParser {
         try {
             var socket = new ws(this.logServerURI)
             socket.onmessage = (msg) => {
-                var event = this.parseEvent(msg.data)
-                this.EventDispatcher.dispatchCallback(event)
+                this.onLine(msg.data)
             }
         }
         catch (e) {
             this.Manager.logger.writeLn(`Remote log server generated an error: ${e.toString()}`)
         }
 
+    }
+    async onLine(line) {
+        line = line.replace(/[^\x20-\x7E]+/g, '')
+        
+        this.Server.Rcon.isRunning = true
+        this.Server.emit('line', line)
+        this.Server.emit('stripped_line', line.trim().replace(new RegExp(/([0-9]+:[0-9]+)\s+/g), ''))
+        var event = this.parseEvent(line)
+        if (!event) return
+
+        this.EventDispatcher.dispatchCallback(event)
     }
 }
 module.exports = EventLogWatcher
