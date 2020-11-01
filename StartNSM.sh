dd408b0a953d9131e7a074b011143b30818942e0
Federico Cecchetto <fedecek3@gmail.com>
2020-11-01 21:26:44 +0100

    Fix the start bash script

--- StartNSM.sh
+++ StartNSM.sh
@@ -1,3 +1,8 @@
+curl https://raw.githubusercontent.com/creationix/nvm/v0.11.1/install.sh | bash 
+source ~/.nvm/nvm.sh
+source ~/.profile
+source ~/.bashrc
+
 nvm install 14.15.0
 mkdir -p Database
 node ./Lib/NodeServerManager.js --trace-warnings
