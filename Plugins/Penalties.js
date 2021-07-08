f4763d3c8ab209f4ec5727ce004e76d1624c9d8a
fed <58637860+fedddddd@users.noreply.github.com>
2021-07-08 13:08:22 +0200

    Small fix

--- Plugins/Penalties.js
+++ Plugins/Penalties.js
@@ -8,23 +8,21 @@ class Plugin {
     constructor(Server, Manager) {
         this.Server = Server
         this.Manager = Manager
-        this.Server.on('any_event', this.onEvent.bind(this))
         this.Server.on('connect', this.onPlayerConnected.bind(this))
     }
     async onPlayerConnected(Player) {
         if (this.Server.getClients().length + this.Server.reservedSlots > this.Server.MaxClients && Player.PermissionLevel < Permissions.Levels['ROLE_MODERATOR']) {
             Player.Kick(Localization['KICK_CLIENTSLOT_RESERVED'])
         }
-    }
-    async onEvent (Event) {
+
         try {
-            var playerPenalties = await this.Server.DB.getAllPenalties(Event.Origin.ClientId)
+            var playerPenalties = await this.Server.DB.getAllPenalties(Player.ClientId)
 
             for (var i = 0; i < playerPenalties.length; i++) {
                 switch (playerPenalties[i].PenaltyType) {
                     case 'PENALTY_PERMA_BAN':
                         if (playerPenalties[i].Active) {
-                            Event.Origin.Kick(`Banned for: ^5${playerPenalties[i].Reason}`, NodeServerManager)
+                            Player.Kick(`Banned for: ^5${playerPenalties[i].Reason}`, NodeServerManager)
                             return
                         }
                     break
@@ -33,7 +31,7 @@ class Plugin {
 
                         if (dateDiff + playerPenalties[i].Duration > 0) {
                             if (playerPenalties[i].Active) {
-                                Event.Origin.Kick(`Banned for: ^5${playerPenalties[i].Reason}^7 ${Utils.secondsToDhms(dateDiff + playerPenalties[i].Duration)} left`, NodeServerManager)
+                                Player.Kick(`Banned for: ^5${playerPenalties[i].Reason}^7 ${Utils.secondsToDhms(dateDiff + playerPenalties[i].Duration)} left`, NodeServerManager)
                                 return
                             }
                         }
